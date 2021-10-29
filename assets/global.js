dbd0fd70a6712846d59b8b7b9843c15e1021baab
frankdev95 <frankdev95@protonmail.com>
2021-10-29 13:41:13 +0100

    Move shop, cart, and contact page

--- assets/global.js
+++ assets/global.js
@@ -1,3 +1,114 @@
+/*
+ * Shopify Common JS
+ *
+ */
+if ((typeof window.Shopify) == 'undefined') {
+  window.Shopify = {};
+}
+
+Shopify.bind = function(fn, scope) {
+  return function() {
+    return fn.apply(scope, arguments);
+  }
+};
+
+Shopify.setSelectorByValue = function(selector, value) {
+  for (var i = 0, count = selector.options.length; i < count; i++) {
+    var option = selector.options[i];
+    if (value == option.value || value == option.innerHTML) {
+      selector.selectedIndex = i;
+      return i;
+    }
+  }
+};
+
+Shopify.addListener = function(target, eventName, callback) {
+  target.addEventListener ? target.addEventListener(eventName, callback, false) : target.attachEvent('on'+eventName, callback);
+};
+
+Shopify.postLink = function(path, options) {
+  options = options || {};
+  var method = options['method'] || 'post';
+  var params = options['parameters'] || {};
+
+  var form = document.createElement("form");
+  form.setAttribute("method", method);
+  form.setAttribute("action", path);
+
+  for(var key in params) {
+    var hiddenField = document.createElement("input");
+    hiddenField.setAttribute("type", "hidden");
+    hiddenField.setAttribute("name", key);
+    hiddenField.setAttribute("value", params[key]);
+    form.appendChild(hiddenField);
+  }
+  document.body.appendChild(form);
+  form.submit();
+  document.body.removeChild(form);
+};
+
+Shopify.CountryProvinceSelector = function(country_domid, province_domid, options) {
+  this.countryEl         = document.getElementById(country_domid);
+  this.provinceEl        = document.getElementById(province_domid);
+  this.provinceContainer = document.getElementById(options['hideElement'] || province_domid);
+
+  Shopify.addListener(this.countryEl, 'change', Shopify.bind(this.countryHandler,this));
+
+  this.initCountry();
+  this.initProvince();
+};
+
+Shopify.CountryProvinceSelector.prototype = {
+  initCountry: function() {
+    var value = this.countryEl.getAttribute('data-default');
+    Shopify.setSelectorByValue(this.countryEl, value);
+    this.countryHandler();
+  },
+
+  initProvince: function() {
+    var value = this.provinceEl.getAttribute('data-default');
+    if (value && this.provinceEl.options.length > 0) {
+      Shopify.setSelectorByValue(this.provinceEl, value);
+    }
+  },
+
+  countryHandler: function(e) {
+    var opt       = this.countryEl.options[this.countryEl.selectedIndex];
+    var raw       = opt.getAttribute('data-provinces');
+    var provinces = JSON.parse(raw);
+
+    this.clearOptions(this.provinceEl);
+    if (provinces && provinces.length == 0) {
+      this.provinceContainer.style.display = 'none';
+    } else {
+      for (var i = 0; i < provinces.length; i++) {
+        var opt = document.createElement('option');
+        opt.value = provinces[i][0];
+        opt.innerHTML = provinces[i][1];
+        this.provinceEl.appendChild(opt);
+      }
+
+      this.provinceContainer.style.display = "";
+    }
+  },
+
+  clearOptions: function(selector) {
+    while (selector.firstChild) {
+      selector.removeChild(selector.firstChild);
+    }
+  },
+
+  setOptions: function(selector, values) {
+    for (var i = 0, count = values.length; i < values.length; i++) {
+      var opt = document.createElement('option');
+      opt.value = values[i];
+      opt.innerHTML = values[i];
+      selector.appendChild(opt);
+    }
+  }
+};
+
+
 function debounce (fn, delay) {
   let timeoutID;
 
@@ -41,6 +152,45 @@ function animationHandler (totalTime, fn) {
       }
   }
 }
+
+const serializeForm = form => {
+  const obj = {};
+  const formData = new FormData(form);
+  for (const key of formData.keys()) {
+    obj[key] = formData.get(key);
+  }
+
+  return JSON.stringify(obj);
+};
+
+function fetchConfig(type = 'json') {
+  return {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json', 'Accept': `application/${type}` }
+  };
+}
+
+class QuantityInput extends HTMLElement {
+  constructor() {
+    super();
+    this.input = this.querySelector('input');
+    this.changeEvent = new Event('change', { bubbles: true })
+
+    this.querySelectorAll('button').forEach(
+      (button) => button.addEventListener('click', this.onButtonClick.bind(this))
+    );
+  }
+
+  onButtonClick(event) {
+    event.preventDefault();
+    const previousValue = this.input.value;
+
+    event.target.name === 'plus' ? this.input.stepUp() : this.input.stepDown();
+    if (previousValue !== this.input.value) this.input.dispatchEvent(this.changeEvent);
+  }
+}
+
+customElements.define('quantity-input', QuantityInput);
 
 function getFocusableElements(container) {
   return Array.from(
---
+++ assets/product-filters-form.js
@@ -0,0 +1,216 @@
+class ProductFiltersForm extends HTMLElement {
+    constructor() {
+        super();
+        this.filterData = [];
+        
+        this.debouncedOnSubmit = debounce((event) => {
+            this.onSubmitHandler(event);
+        }, 500)
+        this.querySelector('form').addEventListener('input', this.debouncedOnSubmit.bind(this));
+    }
+
+    onSubmitHandler(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target.closest('form'));
+        const formDataArray = [...formData];
+
+        const productFilters = formDataArray.filter(filter => filter.some(element => element.includes('product'))).map(filterArr => filterArr[1]).join();
+        const vendorFilters = formDataArray.filter(filter => filter.some(element => element.includes('vendor'))).map(filterArr => filterArr[1]).join();
+        const availabilityFilters = formDataArray.filter(filter => filter.some(element => element.includes('availability'))).map(filterArr => filterArr.join("=")).join("&");
+
+        const optionMaterialFilters = formDataArray.filter(filter => filter.some(element => element.includes('option.material'))).map(filterArr => filterArr[1]).join();
+        const priceFilters = formDataArray.filter(filter => filter.some(element => element.includes('v.price'))).filter(filter => filter[1].length > 0).map(filterArr => [filterArr.join("=")]);
+        const sortFilterURL = formDataArray.filter(filter => filter.some(element => element.includes('sort_by')))[0].join('=');
+
+        const productURL = productFilters.length > 0 ? `filter.p.product_type=${productFilters}` : '';
+        const vendorURL = vendorFilters.length > 0 ? `filter.p.vendor=${vendorFilters}` : '';
+        const optionMaterialURL = optionMaterialFilters.length > 0 ? `filter.v.option.material=${optionMaterialFilters}` : '';
+
+        let priceFilterURL = "";
+
+        if(priceFilters.length > 0) {
+            priceFilterURL = priceFilters.length > 1 ? priceFilters.join('&') : priceFilters[0].toString();
+        }
+
+        const parsedParams = [productURL, vendorURL, optionMaterialURL, priceFilterURL, sortFilterURL].filter(element => element.length > 0);
+        if(availabilityFilters.length > 0) parsedParams.splice(0, 0, availabilityFilters);
+
+        let searchParams = parsedParams;
+
+        if(parsedParams.length > 0) searchParams = parsedParams.join("&");
+
+        this.renderPage(searchParams, event);
+    }
+
+    onActiveFilterClick(e) {
+        e.preventDefault();
+        this.toggleActiveFacets();
+        console.log(e.currentTarget.href);
+        this.renderPage(new URL(e.currentTarget.href).searchParams.toString())
+    }
+
+    toggleActiveFacets(disabled = true) {
+        document.querySelectorAll('.js-facet-remove').forEach(element => {
+            element.classList.toggle('disabled', disable);
+        })
+    }
+
+    renderPage(searchParams, event, updateURLHash = true) {
+        const sections = this.getSections();
+
+        document.getElementById('ProductGrid').querySelector('.collection-products').classList.add('loading');
+        
+        sections.forEach((section) => {
+            const url = `${window.location.pathname}?section_id-${section.section}${searchParams && `&${searchParams}`}`;
+            const filterDataUrl = element => element.url === url;
+
+            this.filterData.some(filterDataUrl) ? this.renderSectionFromCache(filterDataUrl, event) : this.renderSectionFromFetch(url, event);
+        });
+
+        if (updateURLHash) this.updateURLHash(searchParams);
+    }
+
+    renderSectionFromFetch(url, event) {
+        fetch(url)
+            .then(response => response.text())
+            .then(responseText => {
+                const html = responseText;
+                this.filterData = [...this.filterData, { html, url }];
+                this.renderFilters(html, event);
+                this.renderProductGrid(html);
+            });
+    }
+
+    renderSectionFromCache(filterDataUrl, event) {
+        const html = this.filterData.find(filterDataUrl).html;
+        this.renderFilters(html, event);
+        this.renderProductGrid(html);
+    }
+
+    renderProductGrid(html) {
+        const innerHTML = new DOMParser()
+            .parseFromString(html, 'text/html')
+            .getElementById('ProductGrid').innerHTML;
+
+        document.getElementById('ProductGrid').innerHTML = innerHTML;
+    }
+
+    renderFilters(html, event) {
+        const parsedHTML = new DOMParser().parseFromString(html, 'text/html');
+        const filterDetailsElement = parsedHTML.querySelectorAll('#ProductFiltersForm .js-filter, #ProductFiltersFormMobile .js-filter');
+        const matchesIndex = (element) => element.dataset.index === event?.target.closest('.js-filter')?.dataset.index
+        
+        const filtersToRender = Array.from(filterDetailsElement).filter(element => !matchesIndex(element)); // filters that aren't being selected
+        const countsToRender = Array.from(filterDetailsElement).find(matchesIndex);
+        filtersToRender.forEach((element) => {
+           const jsFilter =  document.querySelector(`.js-filter[data-index="${element.dataset.index}"]`);
+           jsFilter.innerHTML = element.innerHTML;
+        });
+
+        this.renderActiveFacets(parsedHTML);
+        this.renderAdditionalElements(countsToRender, parsedHTML);
+
+        if(countsToRender) this.renderCounts(countsToRender, event.target.closest('.js-filter'));
+    }
+
+    renderActiveFacets(html) {
+        const activeFacetElementsSelectors = ['active-filters-desktop', 'active-filters-mobile'];
+
+        activeFacetElementsSelectors.forEach((selector) => {
+            const activeFacetsElement = html.querySelector(selector);
+            if(!activeFacetsElement) return;
+            document.querySelector(selector).innerHTML = activeFacetsElement.innerHTML;
+        });
+
+        this.toggleActiveFacets(false);
+    }
+
+    renderAdditionalElements(countsToRender, html) {
+        const mobileElementSelectors = ['.mobile-filters__open', '.mobile-filters__count']
+
+        mobileElementSelectors.forEach(selector => {
+            document.querySelector(selector).innerHTML = html.querySelector(selector).innerHTML;
+        });
+        
+        document.getElementById('ProductFiltersFormMobile').closest('header-drawer').bindEvents(countsToRender);
+    }
+
+    renderCounts(source, target) {
+        const countElementSelectors = ['.selection-count', '.filters__selected'];
+
+        countElementSelectors.forEach(selector => {
+            const targetElement = target.querySelector(selector);
+            const sourceElement = source.querySelector(selector);
+            
+            if(sourceElement && targetElement) {
+                targetElement.outerHTML = sourceElement.outerHTML;
+            }
+        });
+    }
+
+
+    updateURLHash(searchParams) {
+        history.pushState({searchParams }, '', `${window.location.pathname}${searchParams && '?'.concat(searchParams)}`)
+    }
+
+    getSections() {
+        return [
+            {
+                id: "kendama-main-collection-product-grid",
+                section: document.getElementById("kendama-main-collection-product-grid").dataset.id
+            }
+        ]
+    }
+}
+
+class PriceRange extends HTMLElement {
+    constructor() {
+        super();
+        this.querySelectorAll('input').forEach(input => input.addEventListener('change', this.onRangeChange.bind(this)));
+
+        this.setMinAndMaxValues();
+    }
+
+    onRangeChange(event) {
+        this.adjustToValidValues(event.currentTarget);
+        this.setMinAndMaxValues();
+    }
+
+    setMinAndMaxValues() {
+        const inputs = this.querySelectorAll('input');
+        const minInput = inputs[0];
+        const maxInput = inputs[1];
+        if (maxInput.value) minInput.setAttribute('max', minInput.value);
+        if (minInput.value) maxInput.setAttribute('min', maxInput.value);
+        if (minInput.value == "") maxInput.setAttribute('min', maxInput.getAttribute('min', 0));
+        if (maxInput.value == "") minInput.setAttribute('max', maxInput.getAttribute('max'));
+    }
+
+    adjustToValidValues(input) {
+        const value = Number(input.value);
+        const min = Number(input.getAttribute('min'));
+        const max = Number(input.getAttribute('max'));
+
+        if(value < min) input.value = min;
+        if(value > max) input.value = max;
+    }
+}
+
+class FacetRemove extends HTMLElement {
+    constructor() {
+        super();
+        this.querySelector('a').addEventListener('click', (e) => {
+            e.preventDefault();
+            console.log("laksjd")
+            const form = document.querySelector('product-filters-form');
+            console.log(form);
+            form.onActiveFilterClick(e);
+        })
+
+    }
+}
+
+
+customElements.define('facet-remove', FacetRemove);
+customElements.define('product-filters-form', ProductFiltersForm);
+customElements.define('price-range', PriceRange);
